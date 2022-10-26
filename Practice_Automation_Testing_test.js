03e6fb1b296a9cb42074f1d6f6f45fca8157bcee
Nitish Kumar <nitishk@crowdstaffing.com>
2022-10-27 02:36:31 +0530

    Completed Test Case6

--- Practice_Automation_Testing_test.js
+++ Practice_Automation_Testing_test.js
@@ -1,8 +1,8 @@
+const assert=require('assert');
 Feature('Practice_Automation_Testing');
 
 Scenario('Registration & Login', ({ I, practice_pageobjectPage }) => {
-
-    I.amOnPage('https://practice.automationtesting.in/');
+   I.amOnPage('https://practice.automationtesting.in/');
     I.waitForElement(practice_pageobjectPage.label_MyAccount, 3);
     I.click(practice_pageobjectPage.label_MyAccount);
     I.wait(2);
@@ -13,6 +13,23 @@ Scenario('Registration & Login', ({ I, practice_pageobjectPage }) => {
     I.fillField(practice_pageobjectPage.input_username, 'xyz@gmail.com');
     I.fillField(practice_pageobjectPage.input_loginPassword, 'Test@1234!');
     I.click(practice_pageobjectPage.btn_login);
-
-
 });
+Scenario('6.Home page - Arrivals-Images-Add to Basket', async({ I, practice_pageobjectPage }) => {
+    I.amOnPage('https://practice.automationtesting.in');
+    I.click(practice_pageobjectPage.label_Shop);
+    I.wait(2);
+    I.click(practice_pageobjectPage.label_Home);
+    I.wait(3);
+    let numOfArrivals = await I.grabNumberOfVisibleElements('//img[@sizes="(max-width: 300px) 100vw, 300px"]');
+    console.log(numOfArrivals==3);
+    await assert.equal(numOfArrivals,3);
+    I.wait(3);
+    I.click(practice_pageobjectPage.label_ImageSelenium);
+    I.wait(2);
+    I.click(practice_pageobjectPage.btn_addToBasket);
+    I.wait(3);
+    I.click(practice_pageobjectPage.btn_viewBasket);
+    I.wait(2);
+    I.see('Selenium Ruby');
+    I.see('₹500.00')
+    });
