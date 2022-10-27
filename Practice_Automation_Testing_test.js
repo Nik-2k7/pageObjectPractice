b1af7f98292d0a6f477daa016307b4a6a3eb9021
Nitish Kumar <nitishk@crowdstaffing.com>
2022-10-27 20:06:04 +0530

    requested change implemented

--- Practice_Automation_Testing_test.js
+++ Practice_Automation_Testing_test.js
@@ -1,27 +1,26 @@
 const assert=require('assert');
 Feature('Practice_Automation_Testing');
 
-Scenario('Registration & Login', async({ I, practice_pageobjectPage }) => {
-    I.amOnPage('https://practice.automationtesting.in/');
-    I.waitForElement(practice_pageobjectPage.label_MyAccount, 3);
-    I.click(practice_pageobjectPage.label_MyAccount);
-    I.wait(2);
-    I.fillField(practice_pageobjectPage.input_email, 'xyz@gmail.com');
-    I.fillField(practice_pageobjectPage.input_password, 'Test@1234!');
-    I.click(practice_pageobjectPage.btn_register);
-    I.wait(2);
-    I.fillField(practice_pageobjectPage.input_username, 'xyz@gmail.com');
-    I.fillField(practice_pageobjectPage.input_loginPassword, 'Test@1234!');
-    I.click(practice_pageobjectPage.btn_login);
-});
-
+// Scenario('Registration & Login', async({ I, practice_pageobjectPage }) => {
+//     I.amOnPage('https://practice.automationtesting.in/');
+//     I.waitForElement(practice_pageobjectPage.label_MyAccount, 3);
+//     I.click(practice_pageobjectPage.label_MyAccount);
+//     I.wait(2);
+//     I.fillField(practice_pageobjectPage.input_email, 'xyz@gmail.com');
+//     I.fillField(practice_pageobjectPage.input_password, 'Test@1234!');
+//     I.click(practice_pageobjectPage.btn_register);
+//     I.wait(2);
+//     I.fillField(practice_pageobjectPage.input_username, 'xyz@gmail.com');
+//     I.fillField(practice_pageobjectPage.input_loginPassword, 'Test@1234!');
+//     I.click(practice_pageobjectPage.btn_login);
+// });
 Scenario('2. Home Page with three Arrivals only', async({ I, practice_pageobjectPage }) => {
     I.amOnPage('https://practice.automationtesting.in/');
-    // I.waitForElement(practice_pageobjectPage.label_TestCases, 3);
+    I.waitForElement(practice_pageobjectPage.label_Shop, 3);
     I.click(practice_pageobjectPage.label_Shop);
-    I.wait(2);
+    I.wait(3);
     I.click(practice_pageobjectPage.label_Home);
-    I.wait(2);
+    I.wait(3);
     let numOfArrivals = await I.grabNumberOfVisibleElements('//img[@sizes="(max-width: 300px) 100vw, 300px"]');
     console.log(numOfArrivals==3);
 await assert.equal(numOfArrivals,3);
