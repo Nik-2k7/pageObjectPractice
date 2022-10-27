ff1181d9864a39b35d527c76246b53dcdeb139a1
Nitish Kumar <nitishk@crowdstaffing.com>
2022-10-27 10:31:35 +0530

    Completed Test Cases7

--- Practice_Automation_Testing_test.js
+++ Practice_Automation_Testing_test.js
@@ -1,18 +1,19 @@
+const assert=require('assert');
 Feature('Practice_Automation_Testing');
 
-Scenario('Registration & Login', ({ I, practice_pageobjectPage }) => {
-
-    I.amOnPage('https://practice.automationtesting.in/');
-    I.waitForElement(practice_pageobjectPage.label_MyAccount, 3);
-    I.click(practice_pageobjectPage.label_MyAccount);
+Scenario('7.Home page - Arrivals-Add to Basket with more books', async({ I, practice_pageobjectPage }) => {
+    I.amOnPage('https://practice.automationtesting.in');
+    I.click(practice_pageobjectPage.label_Shop);
     I.wait(2);
-    I.fillField(practice_pageobjectPage.input_email, 'xyz@gmail.com');
-    I.fillField(practice_pageobjectPage.input_password, 'Test@1234!');
-    I.click(practice_pageobjectPage.btn_register);
+    I.click(practice_pageobjectPage.label_Home);
+    I.wait(5);
+    let numOfArrivals = await I.grabNumberOfVisibleElements('//img[@sizes="(max-width: 300px) 100vw, 300px"]');
+    console.log(numOfArrivals==3);
+    await assert.equal(numOfArrivals,3);
+    I.wait(3);
+    I.click(practice_pageobjectPage.label_ImageSelenium);
     I.wait(2);
-    I.fillField(practice_pageobjectPage.input_username, 'xyz@gmail.com');
-    I.fillField(practice_pageobjectPage.input_loginPassword, 'Test@1234!');
-    I.click(practice_pageobjectPage.btn_login);
-
-
-});
+    I.fillField(practice_pageobjectPage.input_noOfBooks, '8706');
+    I.click(practice_pageobjectPage.btn_addToBasket);
+    I.wait(3);
+    });
